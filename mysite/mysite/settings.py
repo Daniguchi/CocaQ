9528ded16c98db46952b9cf769788c896c211c59
Daniguchi <sosehwitch@gmail.com>
2020-07-08 18:57:00 -0400

    Borrar CSS y mas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,6 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = "CocaQ/mysite/Coca/static"
-# or, eg,
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'CocaQ/mysite/Coca/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
