a9957abf0889786e9460518d6abf473155244f60
Daniguchi <sosehwitch@gmail.com>
2020-07-08 17:30:55 -0400

    Cambie el HTML para la página. 3

--- mysite/Coca/views.py
+++ mysite/Coca/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def coca_index(request):
-    return render(request, 'Coca/index.html', {})
+    return render(request, 'Coca/templates/index.html', {})
