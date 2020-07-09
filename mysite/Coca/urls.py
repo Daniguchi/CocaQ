bdbe1ea6655e8f5f0601fa61de548f2604f2c7b3
Daniguchi <sosehwitch@gmail.com>
2020-07-08 21:56:01 -0400

    Integracion HTML

--- mysite/Coca/urls.py
+++ mysite/Coca/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.coca_index, name='Coca_list'),
+
+    path('perfil', views.coca_perfil, name='coca_perfil'),
+    path('inicio', views.coca_inicio, name='inicio'),
+    path('sesion', views.sesion, name='sesion'),
+    path('crear_prueba', views.crear_prueba, name='crear_prueba'),
+    path('crear_reporte', views.crear_reporte, name='crear_reporte'),
+    path('ver_pruebas', views.ver_pruebas, name='ver_pruebas'),
+    path('ver_reportes', views.ver_reportes, name='ver_reportes'),
+    path('trabajadores', views.trabajadores, name='trabajadores'),
+    path('404', views.error, name='error'),
 ]
--- mysite/Coca/views.py
+++ mysite/Coca/views.py
@@ -3,3 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 def coca_index(request):
     return render(request, 'index.html', {})
+
+def sesion(request):
+    return render(request, 'login.html', {})
+
+def coca_inicio(request):
+    return render(request, 'index.html', {})
+
+def coca_perfil(request):
+    return render(request, 'perfil.html', {})
+
+def crear_prueba(request):
+    return render(request, 'crear_prueba.html', {})
+
+def crear_reporte(request):
+    return render(request, 'crear_reporte.html', {})
+
+def ver_reportes(request):
+    return render(request, 'ver_reportes.html', {})
+
+def ver_pruebas(request):
+    return render(request, 'ver_pruebas.html', {})
+
+def trabajadores(request):
+    return render(request, 'trabajadores.html', {})
+
+def error(request):
+    return render(request, '404.html', {})
+
