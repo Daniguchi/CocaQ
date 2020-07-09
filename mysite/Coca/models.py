e76a05cc547e88bc2691cf1b73823acbca16398d
Daniguchi <sosehwitch@gmail.com>
2020-07-09 15:30:53 -0400

    Integracion HTML

--- mysite/Coca/models.py
+++ mysite/Coca/models.py
@@ -10,7 +10,7 @@ class Prueba(models.Model):
     encargado = models.TextField()
     comentario = models.CharField(max_length=200)
     fecha_prueba = models.DateTimeField(
-            blank=True, null=True)
+        blank=True, null=True)
 
     def publish(self):
         self.fecha_prueba = timezone.now()
@@ -20,12 +20,12 @@ class Prueba(models.Model):
         return self.ID_prueba
 
 class Reporte(models.Model):
-    ID_prueba = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    ID_reporte = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     tipo_reporte = models.TextField()
     autor = models.TextField()
     descripcion = models.CharField(max_length=200)
     fecha_reporte = models.DateTimeField(
-            blank=True, null=True)
+        blank=True, null=True)
 
     
 
